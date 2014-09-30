e53b98ca4fae7d9cb64533826b908d0558ab7158
Simon D. Levy <simon.d.levy@gmail.com>
2014-09-29 23:39:09 -0400

    Optionally returns velocity in meters per second.

--- optical_flow.py
+++ optical_flow.py
@@ -28,24 +28,28 @@ optical_flow.py - Optical-flow velocity calculation and display using OpenCV
 import cv
 import sys
 import time
+import math
 
-class OpticalFlow:
+class OpticalFlowCalculator:
     '''
-    A class for optical flow using OpenCV
+    A class for optical flow calculations using OpenCV
     '''
     
-    def __init__(self, frame_width, frame_height, mv_step=16, window_name=None):
+    def __init__(self, frame_width, frame_height, perspective_angle=0, mv_step=16, window_name=None):
         '''
         Creates an OpticalFlow object for images with specified width and height.
-        The mv_step parameter specifies the step size in pixels at which the flow
-        will be sampled.  If window_name is specified, the video and flow will be
-        displayed.
+
+        Optional inputs are:
+
+          perspective_angle - perspective angle of camera, for reporting flow in meters per second
+          mv_step           - step size in pixels for sampling the flow image
+          window_name       - window name for display
         '''
 
         self.mv_step = mv_step
         self.mv_color = (0, 255, 0)
 
-        self.flow = None
+        self.perspective_angle = perspective_angle
 
         self.window_name = window_name
         
@@ -63,10 +67,17 @@ class OpticalFlow:
 
         self.frame_width = frame_width
 
-    def processBytes(self, rgb_bytes):
+        self.prev_time = None
+
+    def processBytes(self, rgb_bytes, distance=None, timestep=1):
         '''
-        Processes one frame of RGB bytes, returning summed X,Y flow
-        '''
+        Processes one frame of RGB bytes, returning summed X,Y flow.
+
+        Optional inputs are:
+
+          distance - distance in meters to image (focal length) for returning flow in meters per second
+          timestep - time step in seconds for returning flow in meters per second
+         '''
 
         self.bgrbytes[0::3] = rgb_bytes[2::3]
         self.bgrbytes[1::3] = rgb_bytes[1::3]
@@ -74,52 +85,75 @@ class OpticalFlow:
                         
         cv.SetData(self.image, self.bgrbytes, self.frame_width*3)
         
-        return self.processFrame(self.image)
+        return self.processFrame(self.image, distance, timestep)
 
-    def processFrame(self, frame):
+    def processFrame(self, frame, distance=None, timestep=1):
         '''
         Processes one image frame, returning summed X,Y flow
+
+        Optional inputs are:
+
+          distance - distance in meters to image (focal length) for returning flow in meters per second
+          timestep - time step in seconds for returning flow in meters per second
         '''
 
         cv.CvtColor(frame, self.gray, cv.CV_BGR2GRAY)
             
-        cv.CalcOpticalFlowFarneback(self.prev_gray, self.gray, self.flow,
-                pyr_scale=0.5, levels=3, winsize=15,
-                iterations=3, poly_n=5, poly_sigma=1.2, flags=0)
+        cv.CalcOpticalFlowFarneback(self.prev_gray, self.gray, self.flow)
 
         xsum, ysum = 0,0
         
         for y in range(0, self.flow.height, self.mv_step):
+
             for x in range(0, self.flow.width, self.mv_step):
+
                 fx, fy = self.flow[y, x]
                 xsum += fx
                 ysum += fy
-                if self.window_name:
-                    cv.Line(frame, (x,y), (int(x+fx),int(y+fy)), self.mv_color)
-                    cv.Circle(frame, (x,y), 1, self.mv_color, -1)
+
+                cv.Line(frame, (x,y), (int(x+fx),int(y+fy)), self.mv_color)
+                cv.Circle(frame, (x,y), 1, self.mv_color, -1)
 
         if self.window_name:
-            thickness = 3
-            ctrx = int(self.flow.width/2)
-            ctry = int(self.flow.height/2)
-            scale = 1. / self.mv_step
-            xsum = int(xsum*scale)
-            ysum = int(ysum*scale)
-            cv.Line(frame, (ctrx, ctry), (ctrx+xsum, ctry), (0,0,255), thickness)
-            cv.Line(frame, (ctrx, ctry), (ctrx, ctry+ysum), (255,0,0), thickness)
             cv.ShowImage(self.window_name, frame)
             if cv.WaitKey(1) == 27:
                 return None
 
         self.prev_gray, self.gray = self.gray, self.prev_gray        
+        
+        # Default to system time if no timestep
+        curr_time = time.time()
+        if not timestep:
+            timestep = (curr_time - self.prev_time) if self.prev_time else 1
+        self.prev_time = curr_time
 
-        return xsum, ysum
+       # Normalize and divide by timestep
+        return  self._get_velocity(xsum, self.flow.width,  distance, timestep), \
+                self._get_velocity(ysum, self.flow.height, distance, timestep)
+
+    def _get_velocity(self, sum_velocity_pixels, dimsize_pixels, distance_meters, timestep_seconds):
+
+        count =  (self.flow.height * self.flow.width) / self.mv_step**2
+
+        average_velocity_pixels_per_second = sum_velocity_pixels / count / timestep_seconds
+
+        return self._velocity_meters_per_second(average_velocity_pixels_per_second, dimsize_pixels, distance_meters) \
+               if self.perspective_angle and distance_meters \
+               else average_velocity_pixels_per_second
+
+    def _velocity_meters_per_second(self, velocity_pixels_per_second, dimsize_pixels, distance_meters):
+
+        distance_pixels = (dimsize_pixels/2) / math.tan(self.perspective_angle/2)         
+
+        pixels_per_meter = distance_pixels / distance_meters
+         
+        return velocity_pixels_per_second / pixels_per_meter
 
 if __name__=="__main__":
 
     capture = cv.CaptureFromCAM(0) if len(sys.argv) < 2 else cv.CaptureFromFile(sys.argv[1])
 
-    sensor = OpticalFlow(int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH)), 
+    flow = OpticalFlowCalculator(int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH)), 
                           int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT)),
                           window_name='Optical Flow')
 
@@ -132,11 +166,11 @@ if __name__=="__main__":
 
         count += 1
             
-        try:
-            if not sensor.processFrame(frame):
-                break
-        except:
+        result = flow.processFrame(frame)
+
+        if not result:
             break
 
     elapsed_sec = time.time() - start_sec
+
     print('%d frames in %3.3f sec = %3.3f frames / sec' % (count, elapsed_sec, count/elapsed_sec))
