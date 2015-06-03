14532b2b3edaf5d0cdb1398b114dbde7bd4155d1
Simon D. Levy <simon.d.levy@gmail.com>
2015-06-03 16:43:01 -0400

    Works with CV2 / Numpy

--- greenball_tracker.py
+++ greenball_tracker.py
@@ -19,42 +19,43 @@ Track a green ball using OpenCV.
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import cv
+import cv2
+import numpy as np
 
-# For OpenCV image display
+# For OpenCV2 image display
 WINDOW_NAME = 'GreenBallTracker' 
 
-def track(bgr_image, threshold=100):
-    '''Accepts BGR image and optional object threshold between 0 and 255 (default = 100).
+def track(image):
+
+    '''Accepts BGR image as Numpy array
        Returns: (x,y) coordinates of centroid if found
                 (-1,-1) if no centroid was found
                 None if user hit ESC
     '''
+
+    # Blur the image to reduce noise
+    blur = cv2.GaussianBlur(image, (5,5),0)
+
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+
+    # Threshold the HSV image for only green colors
+    lower_green = np.array([40,70,70])
+    upper_green = np.array([80,200,200])
+
+    # Threshold the HSV image to get only green colors
+    mask = cv2.inRange(hsv, lower_green, upper_green)
     
-    # Extract bytes, width, and height
-    bgr_bytes = bgr_image.tostring()
-    width = bgr_image.width
-    height = bgr_image.height
-    
-    # Create separate red, green, and blue image matrices from bytes
-    r_image = _create_grayscale_mat(bgr_bytes, width, height, 2)
-    b_image = _create_grayscale_mat(bgr_bytes, width, height, 0)
-    g_image = _create_grayscale_mat(bgr_bytes, width, height, 1)
+    # Blur the mask
+    bmask = cv2.GaussianBlur(mask, (5,5),0)
 
-    # Remove 1/3 of red and blue components from green
-    threes_image = cv.CreateImage((width,height), cv.IPL_DEPTH_8U, 1)  
-    cv.Set(threes_image, 3)
-    _div_and_sub(g_image, r_image, threes_image)
-    _div_and_sub(g_image, b_image, threes_image)
-
-    # Threshold and erode green image
-    cv.Threshold(g_image, g_image, threshold, 255, cv.CV_THRESH_BINARY)
-    cv.Erode(g_image, g_image)
-
-    # Find centroid of eroded image
-    moments = cv.Moments(cv.GetMat(g_image), 1) # binary flag
-    centroid_x = _centroid(moments, 1, 0)
-    centroid_y = _centroid(moments, 0, 1)
+    # Take the moments to get the centroid
+    moments = cv2.moments(bmask)
+    m00 = moments['m00']
+    centroid_x, centroid_y = None, None
+    if m00 != 0:
+        centroid_x = int(moments['m10']/m00)
+        centroid_y = int(moments['m01']/m00)
 
     # Assume no centroid
     ctr = (-1,-1)
@@ -65,47 +66,36 @@ def track(bgr_image, threshold=100):
         ctr = (centroid_x, centroid_y)
 
         # Put black circle in at centroid in image
-        cv.Circle(bgr_image, ctr, 4, (0,0,0))
+        cv2.circle(image, ctr, 4, (0,0,0))
 
     # Display full-color image
-    cv.NamedWindow(WINDOW_NAME)
-    cv.ShowImage(WINDOW_NAME, bgr_image)
+    cv2.imshow(WINDOW_NAME, image)
 
     # Force image display, setting centroid to None on ESC key input
-    if cv.WaitKey(5) == 27:
+    if cv2.waitKey(1) & 0xFF == 27:
         ctr = None
     
     # Return coordinates of centroid
     return ctr
 
-# Removes 1/3 of other_image from src_dst_image
-def _div_and_sub(src_dst_image, other_image, threes_image):
-    cv.Div(other_image, threes_image, other_image) 
-    cv.Sub(src_dst_image, other_image, src_dst_image) 
-
-# Computes centroid based on x, y flags
-def _centroid(moments, x, y):
-    result = None
-    moments00 = cv.GetSpatialMoment(moments, 0, 0)
-    if moments00 != 0:
-        result = int(cv.GetSpatialMoment(moments, x, y) / moments00)
-    return result
-
-# Creates grayscale image from bytes
-def _create_grayscale_mat(bgr_bytes, width, height, channel):
-    image = cv.CreateImageHeader((width,height), cv.IPL_DEPTH_8U, 1)
-    cv.SetData(image, bgr_bytes[channel::3], width)
-    return cv.GetMat(image)
-
 # Test with input from camera
 if __name__ == '__main__':
 
-    capture = cv.CaptureFromCAM(0)
+    capture = cv2.VideoCapture(0)
 
     while True:
-	            
-        if not track(cv.QueryFrame(capture)):
-            break
-      
 
+        okay, image = capture.read()
 
+        if okay:
+
+            if not track(image):
+                break
+          
+            if cv2.waitKey(1) & 0xFF == 27:
+                break
+
+        else:
+
+           print('Capture failed')
+           break
