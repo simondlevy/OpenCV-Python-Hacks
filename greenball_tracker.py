9890b7e8bc28373e5ff03d3e6f33d38488554cf5
Simon D. Levy <simon.d.levy@gmail.com>
2015-06-03 16:43:27 -0400

    Update greenball_tracker.py

--- greenball_tracker.py
+++ greenball_tracker.py
@@ -3,7 +3,7 @@
 '''
 Track a green ball using OpenCV.
 
-    Copyright (C) 2013 Simon D. Levy and Joshua Stough
+    Copyright (C) 2015 Conan Zhao Simon D. Levy
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as 
