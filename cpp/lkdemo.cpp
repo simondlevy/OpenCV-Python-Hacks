14001ad190d1db2cd64815805a65e9a31f144e68
simondlevy <simon.d.levy@gmail.com>
2018-12-14 20:53:17 -0500

    =Cleanup

--- cpp/lkdemo.cpp
+++ cpp/lkdemo.cpp
@@ -27,17 +27,19 @@ int main(int argc, char** argv)
 
     // open the default camera, use something different from 0 otherwise;
     // Check VideoCapture documentation.
-    if(!cap.open(0))
+    if(!cap.open(0)) {
         return 0;
+    }
 
-    for(;;)
-    {
+    while (true) {
+
           Mat frame;
           cap >> frame;
           if( frame.empty() ) break; // end of video stream
           imshow("this is you, smile! :)", frame);
           if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC 
     }
+
     // the camera will be closed automatically upon exit
     // cap.close();
     return 0;
