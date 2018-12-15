a2c6674be74c2241d85e35f12561cc6a0ea6e63f
simondlevy <simon.d.levy@gmail.com>
2018-12-14 20:44:13 -0500

    Begin C++ LK demo

---
+++ cpp/lkdemo.cpp
@@ -0,0 +1,44 @@
+/*
+   lkdemo.cpp - simple C++ demo of Lucas-Kanade optical flow
+
+ Copyright (C) 2018 Simon D. Levy
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as 
+ published by the Free Software Foundation, either version 3 of the 
+ License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+Requires: OpenCV
+*/
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+    VideoCapture cap;
+
+    // open the default camera, use something different from 0 otherwise;
+    // Check VideoCapture documentation.
+    if(!cap.open(0))
+        return 0;
+
+    for(;;)
+    {
+          Mat frame;
+          cap >> frame;
+          if( frame.empty() ) break; // end of video stream
+          imshow("this is you, smile! :)", frame);
+          if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC 
+    }
+    // the camera will be closed automatically upon exit
+    // cap.close();
+    return 0;
+}
